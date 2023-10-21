b2bf21b5ba048238e8a445817fb5aa07ff771d76
josoriom <josoriom93@gmail.com>
2023-10-21 18:00:01 -0500

    add example

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+use hal::{clock::ClockControl, gpio::GpioExt, peripherals::Peripherals, prelude::*, Delay};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let system = peripherals.DPORT.split();
+    let dp = peripherals;
+    let system = dp.DPORT.split();
     let clocks = ClockControl::max(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
-
+    let pins = dp.GPIO.split();
+    let mut builtin_led = pins.gpio2.into_push_pull_output();
     println!("Hello world!");
     loop {
         println!("Loop...");
+        builtin_led.set_high().ok();
+        delay.delay_ms(1500u32);
+        builtin_led.set_low().ok();
         delay.delay_ms(500u32);
     }
 }
